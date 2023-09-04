c7b37045b3020f8e3f0a36dab7258df0c5786ac6
bruno foggia <brunnofoggia@gmail.com>
2023-09-04 18:33:57 -0300

    feat(df): header + footer

--- src/outputs/pdf.ts
+++ src/outputs/pdf.ts
@@ -3,15 +3,17 @@ import { OutputType } from 'types/output';
 import { cloneDeep, defaultsDeep } from 'lodash';
 import { PDFOptions } from 'puppeteer';
 
+// https://pptr.dev/api/puppeteer.pdfoptions
 const defaultConfig: Partial<PDFOptions> = {
     margin: {
-        top: '0px',
+        top: '200px',
         right: '0px',
-        bottom: '0px',
+        bottom: '200px',
         left: '0px',
     },
     printBackground: true,
     format: 'A4',
+    displayHeaderFooter: false,
 };
 
 export class PdfGenerator extends OutputGenerator {
@@ -21,9 +23,13 @@ export class PdfGenerator extends OutputGenerator {
     public async generate(_params: { stream; path; content; config: PDFOptions }) {
         await super.generate({ stream: _params.stream });
 
-        // TODO: split html into header, html, footer ?
         const params = cloneDeep(_params);
         params.config = defaultsDeep({}, _params.config, defaultConfig);
+
+        const { header, body, footer } = this.splitHeaderAndFooterFromHTML(_params.content);
+        this.setHeaderAndFooter(params, header, footer);
+        params.content = body;
+
         params.path = this.buildPath(params.path);
         return this.conversion(params);
     }
@@ -61,5 +67,34 @@ export class PdfGenerator extends OutputGenerator {
         await browser.close();
 
         return { path };
+    }
+
+    splitHeaderAndFooterFromHTML(html) {
+        const headerRegex = /<header>((.|\n|\t|\r)+)<\/header>/g;
+        const header = (html.match(headerRegex)[0] || '').replace(/<\/?header>/g, '').trim();
+        const footerRegex = /<footer>((.|\n|\t|\r)+)<\/footer>/g;
+        const footer = (html.match(footerRegex)[0] || '').replace(/<\/?footer>/g, '').trim();
+        const body = html.replace(headerRegex, '').replace(footerRegex, '');
+
+        return { body, header, footer };
+    }
+
+    setHeaderAndFooter(params, header, footer) {
+        this.setHeader(params, header);
+        this.setFooter(params, footer);
+    }
+
+    setHeader(params, header) {
+        if (header.length) {
+            params.config.displayHeaderFooter = true;
+            params.config.headerTemplate = header;
+        }
+    }
+
+    setFooter(params, footer) {
+        if (footer.length) {
+            params.config.displayHeaderFooter = true;
+            params.config.footerTemplate = footer;
+        }
     }
 }
